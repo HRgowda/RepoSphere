ae35a1133afe9942bc4c2535903bb1d5dbeb3dd2
hemanth18 <hg510443@gmail.com>
2024-12-08 23:06:00 +0530

    Modifications

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,9 +1,59 @@
+"use client"
+import useProject from '@/hooks/use-project'
+import { useUser } from '@clerk/nextjs'
+import { ExternalLink, Github } from 'lucide-react'
 import React from 'react'
+import Link from "next/link"
+import CommitLog from './dashboard/commit-log'
+import AskQuestionCard from './dashboard/ask-question-card'
+import MeetingCard from './dashboard/meeting-card'
 
-const page = () => {
+const Dashboard = () => {
+  const {user} = useUser()
+  const { project } = useProject();
+
   return (
-    <div>Page</div>
+    <div>
+      {/* {project?.id} */}
+      <div className='flex items-center justify-between flex-wrap gap-y-4'>
+        {/* github link */}
+        <div className='w-fit rounded-md bg-primary px-4 py-3'>
+          <div className="flex items center">
+
+            <Github className='size-5 text-white'></Github>
+            <div className='ml-2'>
+              <p className='text-sm font-medium text-white'>
+                This project is linked to {''}              
+                <Link href={project?.githubUrl ?? ""} className='inline-flex items-center text-white/80 hover:underline'>
+                  {project?.githubUrl}
+                  <ExternalLink className='ml-1 size-4'></ExternalLink>
+                </Link>
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="h-4"></div>
+
+      </div>
+
+      <div className="mt-4">
+
+      <div className="grid grid-cols-1 gap-4">
+        <div className="col-span-3">
+          <AskQuestionCard />
+        </div>
+        {/* <div className="col-span-2">
+          <MeetingCard />
+        </div> */}
+      </div>
+      </div>
+
+      <div className="mt-8"></div>
+        <CommitLog></CommitLog>
+    </div>
   )
 }
 
-export default page
+export default Dashboard
